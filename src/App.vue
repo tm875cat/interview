9f5c1f2cb65cee8fc0dc7a329303ffe099398259
tm875cat <tm875leter@gmail.com>
2023-08-24 21:45:38 +0800

    建環境

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Home from './views/Home.vue';
+</script>
+
+<template>
+    <router-view />
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+
