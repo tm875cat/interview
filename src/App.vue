3a8f426cfce9d41698e3248227dcefca2346f0e3
tm875cat <tm875leter@gmail.com>
2023-08-31 00:09:25 +0800

    改變 app 顯示方法

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
 import HelloWorld from "./components/HelloWorld.vue";
+import Home from "./views/Home.vue";
 </script>
-
 <template>
   <router-view />
-  <HelloWorld />
+  <Home />
 </template>
